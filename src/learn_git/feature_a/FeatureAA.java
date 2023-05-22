1885d8cffc66eed94abc2167019bc7fb4e50b384
TInaJ92 <tinatebany@gmail.com>
2023-05-22 15:52:36 -0400

    added new field to provide base implementation

---
+++ src/learn_git/feature_a/FeatureAA.java
@@ -0,0 +1,11 @@
+package learn_git.feature_a;
+
+public class FeatureAA {
+    String name;
+    String id ;
+    FeatureAA(String name, String id){
+        this.name=name;
+        this.id=id;
+    }
+}
+
